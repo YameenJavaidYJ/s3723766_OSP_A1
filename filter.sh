3df5eedee2fa6bbebe4989ddd1707e1258c1f002
s3723766 <s3723766@student.rmit.edu.au>
2022-08-13 14:19:05 +1000

    Updated filter

--- filter.sh
+++ filter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ ! $# -eq 1 ] 
+if [ ! $# -eq 2 ] 
 then 
-    echo "Missing file, usage: 'filter.sh FILENAME'"
+    echo "Missing file, usage: 'filter.sh INPUT_FILENAME OUTPUT_FILENAME'"
     exit 0
 fi
 
 if [ -e $1 ]
 then
-    cat $1 | grep -o -w '\w\{3,15\}' | sort -u > output.txt
+    cat $1 | grep -o -w '\w\{3,15\}' | sort -u > $2
 else
-    echo "File does not exist, usage 'filter.sh FILENAME'"
+    echo "Input file does not exist, usage 'filter.sh INPUT_FILENAME OUTPUT_FILENAME'"
 fi
