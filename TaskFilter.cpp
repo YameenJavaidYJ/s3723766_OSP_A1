3f8376c1f8dfdd64568077955b71fb18c2306047
s3723766 <s3723766@student.rmit.edu.au>
2022-08-14 21:36:17 +1000

    Task1 Duplicate lines - read todo's

--- TaskFilter.cpp
+++ TaskFilter.cpp
@@ -3,11 +3,49 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <list>
+#include <ranges>
 
 bool check_filename (const std::string& name) {
   struct stat buffer;   
   return (stat (name.c_str(), &buffer) == 0); 
 }
+
+bool valid_length(const std::string& string) {
+    if(string.length() < 3 || string.length() > 15) {
+        return false;
+    }
+
+    return true;
+}
+
+bool only_lowercase_chars(const std::string& string) {
+    return string.find_first_not_of("abcdefghijklmnopqrstuvwxyz") == std::string::npos;
+}
+
+bool duplicate_line(const std::string& string, std::string readLines[]) {
+    //TODO Fix this, not working
+    for(int i = 0; i < *(&readLines + 1) - readLines; i++) {
+        if(readLines[i] == string) {
+            return true; 
+        }
+    }
+
+    return false;
+}
+
+int number_of_lines(std::ifstream * inputFile) {
+    int tmp = 0; 
+
+    for(std::string line; std::getline(*inputFile, line);) {
+        tmp++; 
+    }
+
+    return tmp;
+}
+
 
 void TaskFilter(const std::string& input, const std::string& output) {
     if(!check_filename(input)) {
@@ -15,5 +53,18 @@ void TaskFilter(const std::string& input, const std::string& output) {
         return;
     }; 
 
+    std::ifstream InputFile(input); 
     std::ofstream CleanFile(output);
+    std::string readLines[number_of_lines(&InputFile)];
+    
+    for(std::string curLine; std::getline(InputFile, curLine);) {
+        if(!valid_length(curLine)) { continue; }
+        if(!only_lowercase_chars(curLine)) { continue; }
+        if(duplicate_line(curLine, readLines)) { continue; }
+        //TODO No more than 2 conseq
+
+        CleanFile << curLine << "\n";
+    }
+
+    CleanFile.close();
 }
--- makefile
+++ makefile
@@ -1,12 +1,11 @@
 default: all
 
-#Clean the dir (remove output files)
-c:
-	rm -f Task1
-
 #Default action when running make
 all:
 	g++ -Wall -Werror -std=c++14 -g -O -o  Task1 Task1.cpp
 
 task1: 
 	g++ -Wall -Werror -std=c++14 -g -O -o  Task1  TaskFilter.cpp Task1.cpp
+
+task1-c:
+	rm -f Task1
